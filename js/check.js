2ec501d47005c932c4481832d91951e726a75de5
AConchas <43876550+AConchas@users.noreply.github.com>
2019-01-21 11:16:32 +0100

    yjyjhyh

---
+++ js/check.js
@@ -0,0 +1,28 @@
+
+function checkAddress() {
+    if (document.getElementById("category-1")) {
+        
+        
+    }
+
+    if (document.getElementById("category-2")) {
+        
+        
+    }
+
+    if (document.getElementById("category-3")) {
+        
+        
+    }
+
+    if (document.getElementById("category-4")) {
+        
+
+    }
+
+    if (document.getElementById("category-5")) {
+        
+        
+    }
+
+}
