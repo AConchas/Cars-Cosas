dad44d50840b9cf863556d22f5240a2b88e529d3
AConchas <43876550+AConchas@users.noreply.github.com>
2019-01-21 11:50:52 +0100

    asfasdf

--- js/check.js
+++ js/check.js
@@ -16,8 +16,11 @@ function checkAddress() {
     }
 
     if (document.getElementById("category-4")) {
-        
-
+        document.getElementById("category-1").style.display = "none";
+        document.getElementById("category-2").style.display = "none";
+        document.getElementById("category-3").style.display = "none";
+        document.getElementById("category-5").style.display = "none";
+        alert("PAco")
     }
 
     if (document.getElementById("category-5")) {
