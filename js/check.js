cee5abdadf0c3e3d9de74ca3ed317527e1606892
AConchas <43876550+AConchas@users.noreply.github.com>
2019-01-22 08:53:28 +0100

    chechbox

--- js/check.js
+++ js/check.js
@@ -1,5 +1,8 @@
 
 function checkNissan() {
+    if (document.getElementById("category-1").checked || document.getElementById("category-2").checked || document.getElementById("category-3").checked || document.getElementById("category-4").checked || document.getElementById("category-5").checked) {
+        
+    
     if(document.getElementById("category-4").checked){
         document.getElementById('Nissan').style.display = 'block';
     }
@@ -34,6 +37,14 @@ function checkNissan() {
     else{
     document.getElementById('Mustang').style.display = 'none';
     }
+}
+else{
+    document.getElementById('Nissan').style.display = 'block';
+    document.getElementById('Audi').style.display = 'block';
+    document.getElementById('Lambo').style.display = 'block';
+    document.getElementById('BMW').style.display = 'block';
+    document.getElementById('Mustang').style.display = 'block';
+}
 }
 
 
